9c020fdbde8f09287f21c2eff2222477d1ddf6fa
Ddanilo9 <interiordanilo3@gmail.com>
2022-07-22 15:27:33 +0200

    fix bug if

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@ for (let i = 1; i < 101; i++) {
         //se i multipli sono di 5 avranno un output "buzz"
     } else if (i % 5 === 0) {
         console.log('buzz');
-        //si i multipli sono  di 3 e 5  avranno un output "buzzfizz"
-    } else if (i % 3 === 0 && i % 5 === 0) {
+    //se i multipli sono  di 3 e 5  avranno un output "buzzfizz"
+    }  if (i % 3 === 0 && i % 5 === 0) {
         console.log('fizzbuzz');
     } else console.log(i)
 }
