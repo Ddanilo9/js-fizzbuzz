134afc77db04d499f97a8cc02287b1c8ae881553
Ddanilo9 <interiordanilo3@gmail.com>
2022-07-22 16:37:46 +0200

    add element in dom

--- js/app.js
+++ js/app.js
@@ -1,28 +1,36 @@
-
+//recupero il .box dal dom
+const box = document.querySelector('.box')  
 
 //creo un ciclo per creare i numeri da 1 a 100
+
 for (let i = 1; i < 101; i++) {
-//creo un elemento nel dom    
-const elementBody = document.createElement('div')
-elementBody.append(i)
-elementBody.classList.add('container')
-console.log(elementBody)
+    //inserisco un div nel dom
+    const divBody = document.createElement('div')
 
+//se i multipli sono  di 3 e 5  avranno un output "fizzbuzz"
+    if (i % 3 === 0 && i % 5 === 0) {
+        divBody.append('fizzbuzz');
 
-//se i multipli sono di 3 avranno un output "fizz"
-    if (i % 3 === 0 ) {
-        console.log('fizz');
         //se i multipli sono di 5 avranno un output "buzz"
-    } else if (i % 5 === 0) {
-        console.log('buzz');
-    //se i multipli sono  di 3 e 5  avranno un output "buzzfizz"
-    }  if (i % 3 === 0 && i % 5 === 0) {
-        console.log('fizzbuzz');
-    } else console.log(i)
-    
+    }  else if (i % 5 === 0) {
+        divBody.append('buzz');
+
+    //se i multipli sono di 3 avranno un output "fizz"
+    }  else if (i % 3 === 0 ) {
+        divBody.append('fizz');
+        //altrimenti
+    }   else {divBody.append(i)     
+    } 
+    //stampo il div nel dom
+    box.append(divBody)
 }
 
 
+ 
+  
+ 
+  
+ 
 
 
 
