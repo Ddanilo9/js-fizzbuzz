6d9387f561d658e2a3238c9917cc6f33c83066fd
Ddanilo9 <interiordanilo3@gmail.com>
2022-07-22 16:55:08 +0200

    bonus 2

--- js/app.js
+++ js/app.js
@@ -6,20 +6,24 @@ const box = document.querySelector('.box')
 for (let i = 1; i < 101; i++) {
     //inserisco un div nel dom
     const divBody = document.createElement('div')
+    divBody.classList.add('boxitem')
 
 //se i multipli sono  di 3 e 5  avranno un output "fizzbuzz"
     if (i % 3 === 0 && i % 5 === 0) {
         divBody.append('fizzbuzz');
+        divBody.classList.add('m3-5')
 
         //se i multipli sono di 5 avranno un output "buzz"
     }  else if (i % 5 === 0) {
         divBody.append('buzz');
-
+        divBody.classList.add('m-5')
     //se i multipli sono di 3 avranno un output "fizz"
     }  else if (i % 3 === 0 ) {
         divBody.append('fizz');
+        divBody.classList.add('m-3')
         //altrimenti
-    }   else {divBody.append(i)     
+    }   else {divBody.append(i)
+        divBody.classList.add('neutro')     
     } 
     //stampo il div nel dom
     box.append(divBody)
